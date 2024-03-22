8f4dc7df339e44acc032a5beb39aad2a69c5810a
Jasper2002 <46618765+jasper1902@users.noreply.github.com>
2024-03-23 03:06:57 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-"use client";
-import { Card, CardHeader, CardBody, Button } from "@nextui-org/react";
+"use client"
+import { Card, CardHeader, CardBody, Button, Code } from "@nextui-org/react";
 import { useState, useEffect } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -7,39 +7,45 @@ import "react-toastify/dist/ReactToastify.css";
 export default function Home() {
   const [paletteColors, setPaletteColors] = useState<string[]>([]);
 
-  function getRandomColorHex() {
-    let red = Math.floor(Math.random() * 256);
-    let green = Math.floor(Math.random() * 256);
-    let blue = Math.floor(Math.random() * 256);
-
-    let hexRed = red.toString(16).padStart(2, "0");
-    let hexGreen = green.toString(16).padStart(2, "0");
-    let hexBlue = blue.toString(16).padStart(2, "0");
-
-    let hexColor = "#" + hexRed + hexGreen + hexBlue;
-
-    return hexColor;
-  }
-
   useEffect(() => {
     generateRandomPalette();
-    const handleKeyDown = (event: { code: string }) => {
-      if (event.code === "Space") {
-        generateRandomPalette();
-      }
-    };
-    window.addEventListener("keydown", handleKeyDown);
-    return () => {
-      window.removeEventListener("keydown", handleKeyDown);
-    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  function generateRandomPalette() {
-    let colorsArray = [];
-    for (let i = 0; i < 6; i++) {
-      colorsArray.push(getRandomColorHex());
+  useEffect(() => {
+    function handleKeyUp(e: { key: string }) {
+      const key = e.key.toUpperCase();
+      if (key === "C") {
+        copyColorToClipboard(paletteColors.join(" "));
+      }
     }
+
+    function handleKeyDown(e: { key: string }) {
+      if (e.key === " ") {
+        generateRandomPalette();
+      }
+    }
+
+    window.addEventListener("keyup", handleKeyUp);
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keyup", handleKeyUp);
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [paletteColors]);
+
+  function generateRandomPalette() {
+    const colorsArray = Array.from({ length: 6 }, getRandomColorHex);
     setPaletteColors(colorsArray);
+  }
+
+  function getRandomColorHex() {
+    const red = Math.floor(Math.random() * 256).toString(16).padStart(2, "0");
+    const green = Math.floor(Math.random() * 256).toString(16).padStart(2, "0");
+    const blue = Math.floor(Math.random() * 256).toString(16).padStart(2, "0");
+    return `#${red}${green}${blue}`;
   }
 
   function copyColorToClipboard(color: string) {
@@ -90,6 +96,15 @@ export default function Home() {
           <p>
             Or just press the &quot;Spacebar&quot; to generate new palettes.
           </p>
+          <div
+            onClick={() => copyColorToClipboard(paletteColors.join(" "))}
+            className="cursor-pointer"
+          >
+            <Code color="secondary" className="text-center">
+              Click to copy individual color ● Press &quot;C&quot; to copy
+              palette
+            </Code>
+          </div>
         </div>
       </div>
       <ToastContainer />
